cea67e2d18a06e85b73af485f4cb0a46212e4a88
Satori Kagurazaka <komeiji@satori.moe>
2017-08-08 22:45:12 +0800

    增加rs->setid指定session id

--- RediSession.php
+++ RediSession.php
@@ -4,6 +4,8 @@ class RediSession {
 	private static $session_id;
 	private static $expire_time;
 	private static $cookie_name;
+	private static $custom_id;
+	
 	public function __construct($ip, $port, $auth = false, $expire_time = 86400, $cookie_name = 'RedisSESSID') {
 		self::$redis = new Redis();
 		self::$redis->connect($ip, $port);
@@ -13,27 +15,28 @@ class RediSession {
 		if (!self::$redis->ping()) {
 			return false;
 		}
-		self::$session_id = md5(uniqid());
+		self::$session_id = $this->setid('__init');
 		self::$expire_time = $expire_time;
 		self::$cookie_name = $cookie_name;
 	}
-	public function getid() {
-		if (!isset($_COOKIE[self::$cookie_name])) {
-			$session_id = md5(uniqid());
-			setcookie(self::$cookie_name, $session_id, time() + self::$expire_time);
+	public function setid($session_id = false) {
+		if ($session_id == '__init') {
+			self::$custom_id = false;
+			return md5(uniqid());
 		} else {
-			$session_id = $_COOKIE[self::$cookie_name];
+			self::$custom_id = true;
+			self::$session_id = $session_id;
+			return true;
 		}
+
+	}
+	public function getid() {
+		$session_id = $this->GetSessionID();
 		return $session_id;
 	}
 
 	public function set($key, $value) {
-		if (!isset($_COOKIE[self::$cookie_name])) {
-			$session_id = md5(uniqid());
-			setcookie(self::$cookie_name, $session_id, time() + self::$expire_time);
-		} else {
-			$session_id = $_COOKIE[self::$cookie_name];
-		}
+		$session_id = $this->GetSessionID();
 		$data = self::$redis->get($session_id);
 		if (!$data) {
 			$data = [];
@@ -46,12 +49,7 @@ class RediSession {
 	}
 
 	public function unset($key) {
-		if (!isset($_COOKIE[self::$cookie_name])) {
-			$session_id = md5(uniqid());
-			setcookie(self::$cookie_name, $session_id, time() + self::$expire_time);
-		} else {
-			$session_id = $_COOKIE[self::$cookie_name];
-		}
+		$session_id = $this->GetSessionID();
 		$data = self::$redis->get($session_id);
 		if (!$data) {
 			$data = [];
@@ -64,32 +62,27 @@ class RediSession {
 	}
 
 	public function get($key) {
-		if (!isset($_COOKIE[self::$cookie_name])) {
-			if (strlen($_COOKIE[self::$cookie_name]) !== 32) {
-				return false;
-			} else {
-				$session_id = md5(uniqid());
-				setcookie(self::$cookie_name, $session_id, time() + self::$expire_time);
-			}
-		} else {
-			$session_id = $_COOKIE[self::$cookie_name];
-		}
+		$session_id = $this->GetSessionID();
 		$data = json_decode(self::$redis->get($session_id), true);
 		return @$data[$key];
 	}
 
 	public function getAll() {
-		if (!isset($_COOKIE[self::$cookie_name])) {
-			if (strlen($_COOKIE[self::$cookie_name]) !== 32) {
-				return false;
-			} else {
-				$session_id = md5(uniqid());
-				setcookie(self::$cookie_name, $session_id, time() + self::$expire_time);
-			}
-		} else {
-			$session_id = $_COOKIE[self::$cookie_name];
-		}
+		$session_id = $this->GetSessionID();
 		$data = json_decode(self::$redis->get($session_id), true);
 		return $data;
+	}
+	private function GetSessionID() {
+		if (!isset($_COOKIE[self::$cookie_name]) && self::$custom_id == false) {
+			$session_id = md5(uniqid());
+			setcookie(self::$cookie_name, $session_id, time() + self::$expire_time);
+		} else {
+			if (self::$custom_id == true) {
+				$session_id = self::$session_id;
+			} else {
+				$session_id = $_COOKIE[self::$cookie_name];
+			}
+		}
+		return $session_id;
 	}
 }
